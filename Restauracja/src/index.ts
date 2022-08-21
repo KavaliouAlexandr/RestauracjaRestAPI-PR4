c562a6d382c50d1c3cc21829c482bdb03c5eef1f
Alexandr Kavaliou <80467603+KavaliouAlexandr@users.noreply.github.com>
2022-08-21 19:13:43 +0200

    Index

---
+++ Restauracja/src/index.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import { Request, Response } from "express";
+
+
+const mongoose = require("mongoose");
+
+const app = express();
+
+const PORT = 3000;
+
+const dbURI =
+  "";
+
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then((result:any) => console.log("Connected to Mongoose: " + app.listen(3000)))
+    .catch((err:any) => console.log("Failed to connect to Mongoose: " + err))
+
+
+
+
+
+
+app.get("/", function (req: Request, res: Response) {
+  res.send("Witaj w API restauracji");
+});
+
+
