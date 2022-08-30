1a7d4d5b81f89a55ba90b96633ada90466742f12
Alexandr Kavaliou <80467603+KavaliouAlexandr@users.noreply.github.com>
2022-08-30 19:39:56 +0200

    com

--- Restauracja/src/index.ts
+++ Restauracja/src/index.ts
@@ -1,6 +1,12 @@
 import express from "express";
 import { Request, Response } from "express";
-
+import stolikiObj from "../obj/stoliki"
+import restauracjeObj from "../obj/restauracje"
+import pracownikObj from "../obj/pracownik"
+import danieObj from "../obj/danie"
+import rezerwacjaObj from "../obj/rezerwacja"
+import produktObj from "../obj/produkty"
+import zamowienieObj from "../obj/zamowienie"
 
 const mongoose = require("mongoose");
 
@@ -15,7 +21,15 @@ mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
     .then((result:any) => console.log("Connected to Mongoose: " + app.listen(3000)))
     .catch((err:any) => console.log("Failed to connect to Mongoose: " + err))
 
+    app.use(bodyParser.json());
 
+    app.use("/stoliki", stolikiObj);
+    app.use("/restauracje", restauracjeObj);
+    app.use("/pracownicy", pracownikObj);
+    app.use("/dania", danieObj);
+    app.use("/rezerwacje", rezerwacjaObj);
+    app.use("/produkty", produktObj);
+    app.use("/zamowienia", zamowienieObj);
 
 
 
